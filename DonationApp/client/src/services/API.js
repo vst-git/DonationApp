d614cd254b1c7fbcca084a321373938cb956151f
Vivek Singh <106908794+vst-git@users.noreply.github.com>
2023-09-29 18:10:54 +0530

    Update API.js

--- DonationApp/client/src/services/API.js
+++ DonationApp/client/src/services/API.js
@@ -1,21 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = `${window.location.protocol}//${window.location.hostname}/api/v1`;
+const baseURL = `${window.location.protocol}`//${window.location.hostname}/api/v1;
 const API = axios.create({ baseURL: baseURL });
 
 API.interceptors.request.use((req) => {
-  if (localStorage.getItem('token')) {
-    req.headers.Authorization = `Bearer ${localStorage.getItem('token')} `;
+  if (localStorage.getItem("token")) {
+    req.headers.Authorization = `Bearer ${localStorage.getItem("token")} `;
   }
   return req;
 });
 
 export default API;
-
-
-
-
-
-
-
-
