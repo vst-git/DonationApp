263682a2a2c2e0a188bff2648488f2ca3fbad044
Vivek Singh <106908794+vst-git@users.noreply.github.com>
2023-10-01 09:02:36 +0530

    Update Login.js

--- DonationApp/client/src/pages/auth/Login.js
+++ DonationApp/client/src/pages/auth/Login.js
@@ -20,7 +20,6 @@ const Login = () => {
       ) : (
         <div className="row g-0">
           <div className="col-md-8 form-banner">
-            <img src="./assets/images/banner1.jpg" alt="loginImage" />
           </div>
           <div className="col-md-4 form-container">
             <Form formTitle={''} submitBtn={'Login'} formType={'login'} />
